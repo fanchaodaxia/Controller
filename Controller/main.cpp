7d2411c66d98ae065855a04ace6ce325bc1e062c
Chao Fan <cfan@perimetermed.com>
2017-02-10 11:08:49 -0500

    hj
    hjhj

--- Controller/main.cpp
+++ Controller/main.cpp
@@ -5,19 +5,19 @@
 
 int main(int argc, char *argv[])
 {
-    UCHAR* address, status;
+    UCHAR *add, *status;
     UCHAR result;
-    int* value;
+    int *value;
     QApplication a(argc, argv);
     Controller w;
     w.show();
     //w.SendCmd(1,TMCL_MVP,MVP_REL,1,2000);
     w.SendCmd(1,TMCL_ROR,0,1,1000);
-    result = w.GetResult(*address,*status,*value);
-    qDebug() << *address;
+    result = w.GetResult(add,status,value);
+    qDebug() << *add;
     qDebug() << *status;
     qDebug() << *value;
-    qDebug() << "the result" << result;
+    qDebug() << "The result" << result;
     return a.exec();
 }
 
